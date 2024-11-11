bcf26a7d60a86dbc0ead2bcc06787d4899a2d7d4
HotFish <admin@hot.fish>
2024-11-11 15:59:00 -0500

    Replace unsourceable images with public domain image

--- genexamples.sh
+++ genexamples.sh
@@ -2,13 +2,10 @@
 
 
 rm -rf ./examples/sdf_out/
-./pysdfer.py ./examples/tiger_rect.png
-./pysdfer.py ./examples/palm_black.png
-./pysdfer.py ./examples/palm_black_a.png --color-underlay white
-./pysdfer.py ./examples/palm_white_a.png --color-underlay black
+./pysdfer.py ./examples/Goethe.jpg
 ./pysdfer.py ./examples/one_layer.ink.svg 
 ./pysdfer.py ./examples/one_layer.plain.svg --main-color green
 ./pysdfer.py --inklayers --atlas ./examples/multi_layer.ink.svg 
-./pysdfer.py --inklayers --atlas ./examples/shiptest.svg --height 256 # --main-color white
+./pysdfer.py --inklayers --atlas ./examples/shiptest.svg --height 256
 ./pysdfer.py ./examples --inklayers --path-out ./examples/sdf_out/bulk_inklayers
 
